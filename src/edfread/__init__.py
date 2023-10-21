73f35f52e67dd5f2a1072c8de690f0f6093ac1e2
Andrii Kapatsyn <prostoandrey01@gmail.com>
2023-10-21 14:06:11 +0300

    Add possiblity to save h5 files on module level

--- src/edfread/__init__.py
+++ src/edfread/__init__.py
@@ -1,4 +1,4 @@
 """Read eye-tracking information from EyeLink EDF files."""
 
-from edfread.parse import read_edf
+from edfread.parse import read_edf, save_human_understandable as save_h5
 from edfread.edf_read import read_preamble, read_messages, read_calibration
